e85fcc8fa804d4cca984c3d817375292cd90fc26
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-09-19 14:04:08 +0200

    add cors and appusecors

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 import express from "express";
 import menu from "./data/menu.json"  with { type: "json" }
+import cors from "cors"
 const app = express();
+
+app.use(cors({ origin: "http://127.0.0.1:5500"}));
+
 
 app.get("/", (req, res) => {  
 res.send("Accueil");
 });
 
 app.get("/menu", (req, res) => {    
-// const data = { "plate": "Hello World Burger", "description": "Un cheeseburger classique (pain, steak, fromage, salade, sauce).","image": "🍔"  };     
 res.json(menu);
 });
 
 app.listen(3000, () => {  console.log("Serveur lancé sur http://localhost:3000");});
+
