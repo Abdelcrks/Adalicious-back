3518bcb89553d692a1263b59804e7985173fccf4
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-09-23 09:06:13 +0200

    add road post

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cors from "cors"
 const app = express();
 
 app.use(cors({ origin: "http://127.0.0.1:5500"}));
+app.use(express.json())
 
 
 app.get("/", (req, res) => {  
@@ -16,10 +17,24 @@ res.json(menu);
 
 app.get("/menu/:id", (req, res) => {  
     const id = Number(req.params.id);  
-    const plat = data.find(p => p.id === id);  
+    const plat = menu.find(p => p.id === id);  
     if (!plat) return res.status(404).json({ error: `Plat id=${id} non trouvé` });  
     res.json(plat);
     });
-    
+
+
+app.post("/orders", (req, res) => {  
+    console.log("[POST /orders] body reçu:", req.body);  
+    const { id, plate, clientName } = req.body;    
+    if (!id || !plate || !clientName) {    
+    return res.status(400).json({ error: "Champs manquants ou invalides" });  
+    }  
+    console.log(`[COMMANDE REÇUE] id=${id} | plat="${plate}" | client="${clientName}"`);  
+    return res.status(201).json({ ok: true, message: `Commande reçue ${plate} pour ${clientName}`}); 
+     });
+
+     
+
+
 app.listen(3000, () => {  console.log("Serveur lancé sur http://localhost:3000");});
 
